7947ed603e64516100621a207a1fcee8d020f1a5
LeeJungwoo <soulpc123@gmail.com>
2024-03-10 01:02:19 +0900

    update

---
+++ Baekjoon Code Test/반복문/11022 (A+B-8).py
@@ -0,0 +1,8 @@
+import sys
+test_case = 0
+t = int(input())
+for i in range(t):
+        a,b = map(int, sys.stdin.readline().split())
+        test_case += 1
+        c = a+b
+        print(f'Case #{test_case}: {a} + {b} = {c}')
---
+++ Baekjoon Code Test/반복문/2438 (별찍기1).py
@@ -0,0 +1,6 @@
+n = int(input())
+s = "*"
+
+for i in range(1, n+1):
+    result = i * s
+    print(result)
