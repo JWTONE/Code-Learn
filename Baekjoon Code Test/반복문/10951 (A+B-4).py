887dbf92cb2974b1fed2711c8f787944d02f1cea
LeeJungwoo <soulpc123@gmail.com>
2024-03-11 07:08:21 +0900

    update

--- Baekjoon Code Test/반복문/10951 (A+B-4).py
+++ Baekjoon Code Test/반복문/10951 (A+B-4).py
@@ -1,4 +1,6 @@
-import sys
-a,b = map(int, sys.stdin.readline().split())
-
-print(a + b)
+while True:
+    try:
+        a, b = map(int, input().split())
+        print(a + b)
+    except EOFError:
+        break
